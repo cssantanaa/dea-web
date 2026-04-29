03ab3fe936ea32942be83d2c422707674b26fc63
cssaantana <css.carolinessantana@gmail.com>
2026-04-29 17:23:54 -0300

    Adicionando RF25 e desenvolvendo RF26

--- src/modules/barreiras/barriers.module.ts
+++ src/modules/barreiras/barriers.module.ts
@@ -3,11 +3,11 @@
 // import { BarriersController } from './barriers.controller';
 // import { BarriersScheduler } from './barriers.scheduler';
 // import { AuditModule } from '../audit/audit.module';
-// import { CodesModule } from '../codes/codes.module';
+// // import { CodesModule } from '../codes/codes.module';
 // import { EstablishmentsModule } from '../establishments/establishments.module';
 
 // @Module({
-//   imports: [AuditModule, CodesModule, EstablishmentsModule],
+// //   imports: [AuditModule, CodesModule, EstablishmentsModule],
 //   providers: [BarriersService, BarriersScheduler],
 //   controllers: [BarriersController],
 //   exports: [BarriersService],
